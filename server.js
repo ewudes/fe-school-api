8bdd0db2797205f01a2bac68851bdb140bb6eaad
ewudes <ewudes@gmail.com>
2022-02-27 18:33:22 +0400

    implements adding, updating and deleting data

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const artists = [
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let artists = [
     {
         id: 1,
         name: 'Metallica'
@@ -31,7 +35,33 @@ app.get('/artists/:id', function(req, res) {
     });
 
     res.send(artist);
-})
+});
+
+app.post('/artists', function(req, res) {
+    const artist = {
+        id: Date.now(),
+        name: req.body.name
+    };
+
+    artists.push(artist);
+    res.send(artist);
+});
+
+app.put('/artists/:id', function(req, res) {
+    const artist = artists.find(function(artist) {
+        return artist.id === Number(req.params.id);
+    });
+
+    artist.name = req.body.name;
+    res.sendStatus(200);
+});
+
+app.delete('/artists/:id', function(req, res) {
+    artists = artists.filter(function(artist) {
+        return artist.id !== Number(req.params.id);
+    });
+    res.sendStatus(200);
+});
 
 app.listen(3012, function() {
     console.log('API app starting');
