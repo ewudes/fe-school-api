e435effe059181eecf718f66907ebb77a76ef8be
ewudes <ewudes@gmail.com>
2022-03-01 16:48:52 +0400

    refactor using async/await

--- app.js
+++ app.js
@@ -1,94 +1,98 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
 const objectId = require("mongodb").ObjectId;
-
+    
 const app = express();
 const jsonParser = express.json();
-
+  
 const mongoClient = new MongoClient("mongodb://localhost:27017/");
-
-let dbClient;
-
+ 
 app.use(express.static(__dirname + "/public"));
-
-mongoClient.connect(function(err, client){
-    if(err) return console.log(err);
-    dbClient = client;
-    app.locals.collection = client.db("usersdb").collection("users");
-    app.listen(3000, function(){
+ 
+ 
+(async () => {
+     try {
+        await mongoClient.connect();
+        app.locals.collection = mongoClient.db("usersdb").collection("users");
+        await app.listen(3000);
         console.log("Сервер ожидает подключения...");
-    });
-});
-
-app.get("/api/users", function(req, res){
-        
-    const collection = req.app.locals.collection;
-    collection.find({}).toArray(function(err, users){
+    }catch(err) {
+        return console.log(err);
+    } 
+})();
+ 
+ 
+app.get("/api/users", async(req, res) => {
          
-        if(err) return console.log(err);
-        res.send(users)
-    });
-     
+    const collection = req.app.locals.collection;
+    try{
+        const users = await collection.find({}).toArray();
+        res.send(users);
+    }
+    catch(err){return console.log(err);}
+      
 });
-
-app.get("/api/users/:id", function(req, res){
-        
+app.get("/api/users/:id", async(req, res) => {
+         
     const id = new objectId(req.params.id);
     const collection = req.app.locals.collection;
-    collection.findOne({_id: id}, function(err, user){
-               
-        if(err) return console.log(err);
+    try{
+        const user = await collection.findOne({_id: id});
         res.send(user);
-    });
+    }
+    catch(err){return console.log(err);}
 });
-
-app.post("/api/users", jsonParser, function (req, res) {
-       
+    
+app.post("/api/users", jsonParser, async(req, res)=> {
+        
     if(!req.body) return res.sendStatus(400);
-       
+        
     const userName = req.body.name;
     const userAge = req.body.age;
-    const user = {name: userName, age: userAge};
-       
-    const collection = req.app.locals.collection;
-    collection.insertOne(user, function(err, result){
-               
-        if(err) return console.log(err);
-        res.send(user);
-    });
-});
-
-app.put("/api/users", jsonParser, function(req, res){
+    let user = {name: userName, age: userAge};
         
+    const collection = req.app.locals.collection;
+     
+    try{
+        await collection.insertOne(user);
+        res.send(user);
+    }
+    catch(err){return console.log(err);}
+});
+     
+app.delete("/api/users/:id", async(req, res)=>{
+         
+    const id = new objectId(req.params.id);
+    const collection = req.app.locals.collection;
+    try{
+        const result = await collection.findOneAndDelete({_id: id});
+        const user = result.value;
+        res.send(user);
+    }
+    catch(err){return console.log(err);}
+});
+    
+app.put("/api/users", jsonParser, async(req, res)=>{
+         
     if(!req.body) return res.sendStatus(400);
     const id = new objectId(req.body.id);
     const userName = req.body.name;
     const userAge = req.body.age;
-       
+        
     const collection = req.app.locals.collection;
-    collection.findOneAndUpdate({_id: id}, { $set: {age: userAge, name: userName}},
-         {returnDocument: "after" },function(err, result){
-               
-        if(err) return console.log(err);     
+    try{
+        const result = await collection.findOneAndUpdate({_id: id}, { $set: {age: userAge, name: userName}},
+         {returnDocument: "after" });
         const user = result.value;
         res.send(user);
-    });
+    }
+    catch(err){return console.log(err);}
 });
-
-app.delete("/api/users/:id", function(req, res){
-        
-    const id = new objectId(req.params.id);
-    const collection = req.app.locals.collection;
-    collection.findOneAndDelete({_id: id}, function(err, result){
-               
-        if(err) return console.log(err);    
-        let user = result.value;
-        res.send(user);
-    });
-});
-
-process.on("SIGINT", () => {
-    dbClient.close();
+  
+// прослушиваем прерывание работы программы (ctrl-c)
+process.on("SIGINT", async() => {
+     
+    await mongoClient.close();
+    console.log("Приложение завершило работу");
     process.exit();
 });
