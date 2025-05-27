c2bdc4deb63ffb1dbadf2c3a0773d44187668f74
Ewudes <ewudes@gmail.com>
2025-05-27 16:08:46 +0300

    add: routes

---
+++ routes/events.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const {
+  getEvents,
+  getEventById,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+  deleteArchivedEvents
+} = require("../controllers/eventsController");
+
+router.get("/", getEvents);
+router.get("/:id", getEventById);
+router.post("/", authMiddleware, createEvent);
+router.put("/", authMiddleware, updateEvent);
+router.delete("/:id", authMiddleware, deleteEvent);
+router.delete("/archive/delete", authMiddleware, deleteArchivedEvents);
+
+module.exports = router;
